e3bf8d6d29180f9757f073b8291f3dbdda89c1f4
Ludson Almeida <ludson@mane.com.vc>
2025-11-21 14:46:35 -0300

    asdask

--- src/hooks/usePublicAreasByUnit.ts
+++ src/hooks/usePublicAreasByUnit.ts
@@ -3,7 +3,7 @@
 
 import * as React from 'react';
 import { apiGet } from '@/lib/api';
-import { resolvePhotoUrl } from '@/lib/assets';
+import { resolvePhotoUrl } from '../lib/assets';
 
 export type AreaOption = {
   id: string;
